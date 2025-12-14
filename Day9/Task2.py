a3009314026ba57fef35dc9b0d064644a2b8a766
P0midor <oberwankacper@gmail.com>
2025-12-14 21:01:46 +0100

    working D9T2

--- Day9/Task2.py
+++ Day9/Task2.py
@@ -32,8 +32,39 @@ def CornersInside(point_1, point_2, polygon):
             return False
     return True
 
+def polygonNotCrossingSquare(point_1, point_2, polygon):
+    up = [[min(point_1[0], point_2[0]), max(point_1[1], point_2[1])], [max(point_1[0], point_2[0]), max(point_1[1], point_2[1])]]
+    right = [[max(point_1[0], point_2[0]), min(point_1[1], point_2[1])], [max(point_1[0], point_2[0]), max(point_1[1], point_2[1])]]
+    left = [[min(point_1[0], point_2[0]), min(point_1[1], point_2[1])], [min(point_1[0], point_2[0]), max(point_1[1], point_2[1])]]
+    down = [[min(point_1[0], point_2[0]), min(point_1[1], point_2[1])], [max(point_1[0], point_2[0]), min(point_1[1], point_2[1])]]
 
-with open("testinput.txt", "r", encoding="utf-8") as file:
+    n = len(polygon)
+    j = n - 1
+    for i in range(n):
+        xi, yi = polygon[i][0], polygon[i][1]
+        xj, yj = polygon[j][0], polygon[j][1]
+
+        if xi == xj:
+            #pionowa
+            if up[0][0] < xi < up[1][0]:
+                if min(yi,yj) < up[0][1] <= max(yi, yj):
+                    return False
+            if down[0][0] < xi < down[1][0]:
+                if min(yi, yj) <= down[0][1] < max(yi, yj):
+                    return False
+        if yi == yj:
+            #pozioma
+            if left[0][1] < yi < left[1][1]:
+                if min(xi,xj) <= left[0][0] < max(xi,xj):
+                    return False
+            if right[0][1] < yi < right[1][1]:
+                if min(xi,xj) < right[0][0] <= max(xi,xj):
+                    return False
+        j = i
+
+    return True
+
+with open("input.txt", "r", encoding="utf-8") as file:
     polygon = file.read().splitlines()
 
 n = len(polygon)
@@ -48,8 +79,9 @@ maxfield = 0
 for point1 in range(n):
     for point2 in range(point1 + 1, n):
         field = (abs(polygon[point1][0] - polygon[point2][0]) + 1) * (abs(polygon[point1][1] - polygon[point2][1]) + 1)
-        if field > maxfield and CornersInside(polygon[point1], polygon[point2], polygon):
-            maxfield = field
+        if field > maxfield:
+            if CornersInside(polygon[point1], polygon[point2], polygon) and polygonNotCrossingSquare(polygon[point1], polygon[point2], polygon):
+                maxfield = field
 
 
 
